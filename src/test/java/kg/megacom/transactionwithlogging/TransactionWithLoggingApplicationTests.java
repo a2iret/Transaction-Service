f5066bc75503363cfdef8b979f944059953cb2be
Aziret java <jut.su312@gmail.com>
2024-04-22 19:02:11 +0600

    first commit

---
+++ src/test/java/kg/megacom/transactionwithlogging/TransactionWithLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megacom.transactionwithlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionWithLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
